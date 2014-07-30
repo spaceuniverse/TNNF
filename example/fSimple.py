544b45bf27007f57fd289dd9a1e6da3ae5f9e156
spaceuniverse <neverdraw@gmail.com>
2014-07-30 19:30:54 +0300

    rename + example folder
    some simple example

---
+++ example/fSimple.py
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------------------#
+# External libraries
+# ---------------------------------------------------------------------#
+
+
+import numpy as np
+import unittest
+import os
+import sys
+sys.path.append('./CORE')
+sys.path.append('../CORE')
+from fTheanoNNclassCORE import OptionsStore, TheanoNNclass, NNsupport, FunctionModel, LayerNN
+from fDataWorkerCORE import csvDataLoader
+
+
+# ---------------------------------------------------------------------#
+
+
+print "FINISH"
+
+
+# ---------------------------------------------------------------------#
