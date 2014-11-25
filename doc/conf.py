cfdfb25e175116c09fc1dda2484dbed15b6be055
Delicate_aRt <roman.grubnyk@gmail.com>
2014-11-25 11:46:39 +0200

    Fixing configuration for RTD integration.

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,8 @@ import mock
 
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'numpy.linalg', 'matplotlib.pylab',
                 'theano', 'theano.tensor.shared_randomstreams', 'theano.tensor',
-                'matplotlib.pyplot', 'pylearn2', 'pylearn2.sandbox.cuda_convnet.filter_acts']
+                'matplotlib.pyplot', 
+                'pylearn2', 'pylearn2.sandbox', 'pylearn2.sandbox.cuda_convnet', 'pylearn2.sandbox.cuda_convnet.filter_acts']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
