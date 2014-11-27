867e75800b2d1b1ca2ea6f03c21cb8f37ee58f62
ss-ds <bike2live@ya.ru>
2014-11-27 20:25:22 +0200

    Workaround for __init__() documentation.

--- doc/conf.py
+++ doc/conf.py
@@ -38,6 +38,8 @@ if on_rtd:
     for mod_name in MOCK_MODULES:
         sys.modules[mod_name] = mock.Mock()
 
+#Trying to include __init__() description in class description
+autoclass_content = 'both'
 #-----------------#
 
 # If extensions (or modules to document with autodoc) are in another directory,
